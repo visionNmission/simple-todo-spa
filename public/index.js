be38fbdd39e4431f11b5a2be5dfa749864418835
Kim Seungha <seungha.me@gmail.com>
2017-09-01 15:20:05 +0900

    완료, 삭제 기능 추가

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 (function(window, document) {
 
-  function render({target, templatePath, dataPath, queryFrom}) {
+  function render({target, templatePath, dataPath, queryFrom = document}) {
     // 템플릿 가져오기
     const templatePromise = axios.get(templatePath)
 
@@ -16,7 +16,6 @@
         })
 
         // 렌더링 결과를 문서에 주입하기
-        queryFrom = queryFrom || document
         const targetEl = queryFrom.querySelector(target)
         targetEl.innerHTML = html
         return targetEl
@@ -26,17 +25,41 @@
   }
 
   function loadTodos() {
+    console.log('start loadTodos')
     render({
       target: '#todos',
       templatePath: '/templates/todos.ejs',
       dataPath: '/api/todos'
+    }).then(todosEl => {
+      todosEl.querySelectorAll('.todo-item').forEach(todoItem => {
+        const id = todoItem.dataset.id
+        
+        // 체크박스 클릭시
+        const checkboxEl = todoItem.querySelector('.todo-checkbox')
+        checkboxEl.addEventListener('click', e => {
+          axios.patch(`/api/todos/${id}`, {
+            complete: e.currentTarget.checked
+          }).then(res => {
+            loadTodos()
+          })
+        })
+
+        // 삭제 아이콘 클릭시
+        const removeLink = todoItem.querySelector('.todo-remove')
+        removeLink.addEventListener('click', e => {
+          axios.delete(`/api/todos/${id}`).then(res => {
+            loadTodos()
+          })
+        })
+      })
     })
   }
 
   document.querySelector('#todo-form').addEventListener('submit', e => {
     e.preventDefault()
+    const form = e.currentTarget
     axios.post('/api/todos', {
-      title: e.currentTarget.elements.title.value
+      title: form.elements.title.value
     })
       .then(loadTodos)
       .then(() => {
